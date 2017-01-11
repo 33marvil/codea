7ca5f9b83f24e6536573a1895060e14fa4e755e8
Martin Ramirez <martinramirezcruz@gmail.com>
2017-01-11 17:51:24 -0600

    FizzBuzz completado

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,21 @@
-def fizzbuzz(1, 8) #metodo
+#metodo cancion
+def fizzbuzz(min, max)# argumento de enteros que toma de parametros de array 
   sing = []#array vacio
-  min.each do |fizz|
-    # max. do |buzz|
-    #   sing << [fizz, buzz]  
-    # end
+  #establece rango, iterar en cada parametro del argumento
+  (min..max).each do |num|
+    #definir variable num
+    if num % 3 == 0 && num % 5 == 0 #si num cumple con multiplo % de 3 && si cumple con multiplo de 5  
+      sing << "FizzBuzz" #empujar resultado y guardar
+    elsif num % 3 == 0 # o si num cumple con % multiplo de 3
+      sing << "Fizz"#empujar resultado y guardar
+    elsif num % 5 == 0#o si num cumple con % multiplo de 5
+      sing << "Buzz"#empujar resultado y guardar 
+    else #si NO cumple variable num con anteriores
+      sing << num  #empujar resultado encontrado
+    end 
   end
-  sing
+  # 
+  sing 
 end
 
 p fizzbuzz(3, 5) == ["Fizz", 4, "Buzz"]
