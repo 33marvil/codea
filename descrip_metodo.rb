e873ce81e56ef8e18dc25eb08e0ea0f9f9bf94e9
Martin Ramirez <martinramirezcruz@gmail.com>
2017-01-11 11:59:05 -0600

    completado

---
+++ descrip_metodo.rb
@@ -0,0 +1,38 @@
+#metodos.rb
+ 
+# Definición de un método
+def hello  
+  puts "Hola"  
+ end  
+ #uso del método
+ hello  
+
+ # Metodo con un argumento
+def hello1(nombre)
+  puts 'Hola' + nombre
+  return 'correcto'
+end
+puts (hello1('Pedro'))
+
+# Método con argumento (sin paréntesis)
+def hello2 nombre2
+  puts 'Hola ' + nombre2
+  return 'correcto'  
+end
+puts (hello2 'Juan' )
+
+
+a = "En una lugar de la mancha"
+ 
+#método sin bang: el objeto no se modifica
+b = a.upcase
+puts b
+puts a
+ 
+#método con bang: el objeto se modifica
+c = a.upcase!
+puts c
+puts a
+
+
+
--- formando_arrays.rb
+++ formando_arrays.rb
@@ -14,13 +14,13 @@
 def array_index(letters, sum) #se define metodo de un array de string mas un integer
   space = [] #se crea un array vacio para colocar el resultado
   letters.each do |vowel| #itera en el array con el string
-   sum.times do |repeat| #despues de iterar se 
-    space << [vowel, repeat +1]
+   sum.times do |repeat| #despues de iterar .times es un loop es decir cuenta las veces indicada por el integer
+    space << [vowel, repeat +1] #en la variable vacia space, se mete en un array el resultado de la iteracion y el loop mas 1, hasta llegar a el numero del integer
    end 
   end
   space  
 end
 
-#p array_index(["c", "b", "a"], 2) #== [["c", 1], ["c", 2], ["b", 1], ["b", 2], ["a", 1], ["a", 2]]
-#p array_index(["a"], 3) #== [["a", p array_index(["chevy", "mustang", "GM",]  rojo)1], ["a", 2], ["a", 3]] 
-p array_index(["chevy", "mustang", "GM"], 3)
+p array_index(["c", "b", "a"], 2) == [["c", 1], ["c", 2], ["b", 1], ["b", 2], ["a", 1], ["a", 2]]
+p array_index(["a"], 3) == [["a", 1], ["a", 2], ["a", 3]]  
+#p array_index(["chevy", "mustang", "GM"], 3)
