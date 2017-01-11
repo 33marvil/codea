07005992b360ce8b5961ab13dc485f5f39ac1363
Martin Ramirez <martinramirezcruz@gmail.com>
2017-01-11 16:38:26 -0600

    archivos de la tarde

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(1, 8) #metodo
+  sing = []#array vacio
+  min.each do |fizz|
+    # max. do |buzz|
+    #   sing << [fizz, buzz]  
+    # end
+  end
+  sing
+end
+
+p fizzbuzz(3, 5) == ["Fizz", 4, "Buzz"]
+p fizzbuzz(10, 15) == ["Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+
+
