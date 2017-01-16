c7b558a740d74111a95c427473c1d3df4a7b45a4
Martin Ramirez <martinramirezcruz@gmail.com>
2017-01-16 13:45:09 -0600

    refactorizacion con Unless

---
+++ refacto_unless.rb
@@ -0,0 +1,13 @@
+def swim(time)
+  unless time > 8
+  end
+    
+  # end
+  # if time < 10
+  #   "#{time} seconds... New Record for 50 meters"
+  # end
+end
+
+#test
+p swim(5) == "5 seconds... New Record for 50 meters"
+p swim(8) == "8 seconds... New Record for 50 meters"
