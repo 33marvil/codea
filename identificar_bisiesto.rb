da183652e8c9650f561f413ee4eab9b425614f90
Martin Ramirez <martinramirezcruz@gmail.com>
2017-01-17 13:25:28 -0600

    bisiesto

--- identificar_bisiesto.rb
+++ identificar_bisiesto.rb
@@ -1,11 +1,18 @@
 def leap_year?(year)
-  
-  
+  if year % 4 == 0 && year % 400 == 0 && year % 100 == 0
+    true
+  elsif year % 100 == 0
+    false
+  elsif year % 400 == 0
+    true
+  elsif year % 4 == 0
+    true
+  else
+    false     
+  end
 end
 
-
 # Pruebas
-
 p leap_year?(1900) == false
 p leap_year?(1988) == true
 p leap_year?(1989) == false
