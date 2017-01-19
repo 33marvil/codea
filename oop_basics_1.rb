7cd67964ac02477bccbead86bfbcf03c63eac43d
Martin Ramirez <martinramirezcruz@gmail.com>
2017-01-19 10:00:53 -0600

    completado

--- oop_basics_1.rb
+++ oop_basics_1.rb
@@ -12,57 +12,210 @@ end
 class Dolphin
 end
 #*******************************************************************************
-class Cat
-  # def initialize(name) """"puedo omitri en este momento""""       
-  # end
-  def maulla #Definir metodo
-    puts "Gato bodegero, miauuuuu"
-  end
+# class Cat
+#   # def initialize(name) """"puedo omitri en este momento""""       
+#   # end
+#   def maulla #Definir metodo
+#     puts "Gato bodegero, miauuuuu"
+#   end
 
-  def vida_nocturna#Definir metodo
-    puts "Gato bodegero, trabaja de noche"    
-  end
+#   def vida_nocturna#Definir metodo
+#     puts "Gato bodegero, trabaja de noche"    
+#   end
 
-  def pardo
-    puts "En la noche, todos los gatos son pardos"    
-  end
-end
+#   def pardo
+#     puts "En la noche, todos los gatos son pardos"    
+#   end
+# end
 
-clase = Cat.new# Se crea variable = a "Clase.new"
-clase.maulla#Se instancia llamando variable + .metodo
-clase.vida_nocturna #Se instancia variable + .metodo
+# clase = Cat.new# Se crea variable = a "Clase.new"
+# clase.maulla#Se instancia llamando variable + .metodo
+# clase.vida_nocturna #Se instancia variable + .metodo
 
-clase_2 = Cat.new # Se crea 2da variable = a "Clase.new"
-clase_2.vida_nocturna #Se instancia variable + .metodo
-clase_2.maulla#Se instancia llamando variable + .metodo
+# clase_2 = Cat.new # Se crea 2da variable = a "Clase.new"
+# clase_2.vida_nocturna #Se instancia variable + .metodo
+# clase_2.maulla#Se instancia llamando variable + .metodo
 
-clase_3 = Cat.new #Se crea variable = "Clase.new"
-clase_3.vida_nocturna
-clase_3.pardo
+# clase_3 = Cat.new #Se crea variable = "Clase.new"
+# clase_3.vida_nocturna
+# clase_3.pardo
 #*************************************************************************************
-class Car
-  def sport# Definir metodo
-    puts "El color rojo es el mas vistoso!"
-  end
+# class Car
+#   def initialize    
+#   end
+#   def sport# Definir metodo
+#     puts "El color rojo es el mas vistoso!"
+#   end
 
-  def cheyene# Definir metodo
-    puts "Es para el rancho"
-  end
+#   def cheyene# Definir metodo
+#     puts "Y la cheyene apa? - ¡Es para el rancho!"
+#   end
 
-  def vocho #definir metoso
-    puts "Va y viene de la escuela"    
+#   def vocho #definir metoso
+#     puts "Va y viene de la escuela"    
+#   end
+# end
+
+# mi_nabe = Car.new
+# mi_nabe.sport
+
+# mi_nabe = Car.new
+# mi_nabe.cheyene
+# mi_nabe.sport
+
+# mi_nabe = Car.new
+# mi_nabe.vocho
+# mi_nabe.cheyene
+# # *******************************************************************************************
+
+# class Window
+#   def initialize    
+#   end
+#   def aumados
+#     puts "Los vidros aumados van el las ventanas del 2do piso"    
+#   end
+
+#   def claros_inastillables
+#     puts "Las ventanas de la sala y comedor son de vidrios inastillable"
+#   end
+
+#   def azules
+#     puts "Las ventanas y cnaceles del baño son de vidrios azulez"
+    
+#   end
+# end
+
+# ventana = Window.new
+# ventana.aumados
+
+# ventana = Window.new
+# ventana.claros_inastillables
+
+# ventana = Window.new
+# ventana.azules
+# *******************************************************************************************************
+# class Water
+#   def initialize    
+#   end
+
+#   def vida
+#     puts "El agua es esencial para todos los seres vivos"    
+#   end
+#   def potable
+#     puts "Es agua es un recurso no renobable y el agua potable"    
+#   end
+
+#   def costo
+#     puts "Nadie sabe el valor del agua hasta que el pozo esta seco. ¡Cuida el agua!"  
+#   end
+# end
+
+# vida = Water.new
+# vida.vida
+
+# embotellada = Water.new
+# embotellada.vida
+# embotellada.potable
+
+# valor = Water.new
+# valor.costo
+# **********************************************************************************************************
+
+# class Fruit
+#   def initialize    
+#   end
+
+#   def temporada
+#     puts "La fruta de temporada es de la mejor calidad"    
+#   end
+
+#   def concerva
+#     puts "Las frutas en concerva te permite comer la frutas aun si no esta en temporada"    
+#   end
+
+#   def casera
+#     puts "Tu puedes hacer mermelada casera con fruta de temporada y tenerla en concerva"    
+#   end
+# end
+
+# que_comprar = Fruit.new
+# que_comprar.temporada
+
+# antojo = Fruit.new
+# antojo.temporada
+# antojo.concerva
+
+# ahorro = Fruit.new
+# ahorro.temporada
+# ahorro.concerva
+# ahorro.casera
+# *****************************************************************
+# class Vegetable
+#   def initialize
+#   end
+#   def dieta
+#     puts "Incluye los vegetales en tu dieta diaria"    
+#   end
+
+#   def comidas
+#     puts "Los vegetables en tus comidas complementan tu nutrición"    
+#   end
+
+#   def botana
+#     puts "Las zanahorias y pepinos son una buena botana de vegetales"
+#   end
+# end
+
+# que_puedo_comer = Vegetable.new
+# que_puedo_comer.dieta
+# que_puedo_comer.botana
+
+# lunch = Vegetable.new
+# lunch.comidas
+# lunch.botana
+
+# mide_tu_cintura = Vegetable.new
+# mide_tu_cintura.dieta
+# mide_tu_cintura.comidas
+# mide_tu_cintura.botana
+#**************************************************************************************
+class Person
+  def initialize
   end
+  def jovenes
+    puts "Los jovenes son las personas que inspiran a mejorar"
+  end
+  def adultos
+    puts "La adultes es una buena combinación de experiencia y ganas de aprender"
+  end
+  def ancianos
+    puts "Eres buen joven? ¡Seras buen anciano!"    
+  end     
 end
+ 
+ vida_productiva = Person.new
+ vida_productiva.jovenes
+ vida_productiva.adultos
+ vida_productiva.ancianos
 
-mi_nabe = Car.new
-mi_nabe.sport
 
-mi_nabe = Car.new
-mi_nabe.cheyene
-mi_nabe.sport
 
-mi_nabe = Car.new
-mi_nabe.vocho
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
